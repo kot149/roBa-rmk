4e90b91fe08d9dbd379e015ac873ca1a95544b03
kot149 <131101078+kot149@users.noreply.github.com>
2025-11-30 20:06:45 +0900

    Add battery level reporting configs

--- src/central.rs
+++ src/central.rs
@@ -2,13 +2,15 @@
 #![no_std]
 
 use rmk::macros::rmk_central;
-use roBa_rmk::BleConnectionLed;
+use roBa_rmk::{xiao_ble_enable_vbat_reading, BleConnectionLed};
 use embassy_nrf::gpio::{Output, Level, OutputDrive};
 
 #[rmk_central]
 mod keyboard_central {
     #[controller(event)]
     fn ble_connection_led() -> BleConnectionLed {
+        xiao_ble_enable_vbat_reading();
+
         let led_blue = Output::new(p.P0_06, Level::High, OutputDrive::Standard);
         let led_red = Output::new(p.P0_26, Level::High, OutputDrive::Standard);
         BleConnectionLed::new(led_blue, led_red)
