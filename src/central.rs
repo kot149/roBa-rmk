22cc1cdc4e8df199ade711788d620a7fdcf07c35
kot149 <131101078+kot149@users.noreply.github.com>
2025-12-17 18:43:45 +0900

    Comment out ble connection led for central

--- src/central.rs
+++ src/central.rs
@@ -2,15 +2,15 @@
 #![no_std]
 
 use rmk::macros::rmk_central;
-use roba_rmk::{BleConnectionLed};
-use embassy_nrf::gpio::{Output, Level, OutputDrive};
+// use roba_rmk::{BleConnectionLed};
+// use embassy_nrf::gpio::{Output, Level, OutputDrive};
 
 #[rmk_central]
 mod keyboard_central {
-    #[controller(event)]
-    fn ble_connection_led() -> BleConnectionLed {
-        let led_blue = Output::new(p.P0_06, Level::High, OutputDrive::Standard);
-        let led_red = Output::new(p.P0_26, Level::High, OutputDrive::Standard);
-        BleConnectionLed::new(led_blue, led_red)
-    }
+    // #[controller(event)]
+    // fn ble_connection_led() -> BleConnectionLed {
+    //     let led_blue = Output::new(p.P0_06, Level::High, OutputDrive::Standard);
+    //     let led_red = Output::new(p.P0_26, Level::High, OutputDrive::Standard);
+    //     BleConnectionLed::new(led_blue, led_red)
+    // }
 }
